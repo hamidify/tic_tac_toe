9a6c111ac150dd60f3520ec5b20a69b84056ad79
Abdulahamid Abdo <abdoabdulhamid@gmail.com>
2018-08-21 21:46:38 +0300

    Calculated winner and declared it.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Square } from "./Square";
+import { calculateWinner } from "../helpers";
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -11,6 +12,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
       squares: squares,
@@ -30,8 +34,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player is : " + (this.state.xIsNext ? "X" : "O");
-
+    let status;
+    const winner = calculateWinner(this.state.squares);
+    if (winner) {
+      status = "Winner is : " + winner;
+    } else {
+      status = "Next player is : " + (this.state.xIsNext ? "X" : "O");
+    }
     return (
       <div>
         <div className="status">{status}</div>
